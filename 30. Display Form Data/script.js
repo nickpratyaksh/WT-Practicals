3f577a0de2e61615bb7d2351cc0f6b5e9a09157e
nickpratyaksh <nickpratyaksh@gmail.com>
2023-04-17 12:49:17 +0530

    final commit

---
+++ 30. Display Form Data/script.js
@@ -0,0 +1,5 @@
+function display(){
+    document.getElementById('oname').innerText = document.getElementById('name').value
+    document.getElementById('omail').innerText = document.getElementById('email').value
+    document.getElementById('ophone').innerText = document.getElementById('phno').value
+}
